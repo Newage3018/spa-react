f696c9ab610d7d4455ef0405f491b2770e098a93
igor artyushin <artvid29@gmail.com>
2020-06-19 14:29:53 +0300

    build one

---
+++ build/precache-manifest.c56fa164f4e6754dbd35e96d046f4ba7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a283fea7235f4d53d66881428a7d1715",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ef891286aa95e449812",
+    "url": "/static/css/main.6a2a759a.chunk.css"
+  },
+  {
+    "revision": "95d9c9e0f61ac4905a2b",
+    "url": "/static/js/2.10b34be7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.10b34be7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ef891286aa95e449812",
+    "url": "/static/js/main.a17b6022.chunk.js"
+  },
+  {
+    "revision": "37d73873e39f000d90dc",
+    "url": "/static/js/runtime-main.f080b327.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
