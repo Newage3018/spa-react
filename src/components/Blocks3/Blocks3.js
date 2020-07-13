f94af68ecd7cb9c4dcd64e411afec17c02c6f8d3
Igor <artvid29@gmail.com>
2020-07-13 22:33:20 +0300

    running

---
+++ src/components/Blocks3/Blocks3.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './Blocks3.css';
+
+function Blocks3() {
+  return (
+    <div className="blocks3_flex">
+        <div className="first_item">
+            <h3>what</h3>
+            <h3>how</h3>
+            <h3>when</h3>
+        </div>
+        <div className="first_item"></div>
+        <div className="first_item"></div>
+        
+    </div>
+  )
+}
+
+export default  Blocks3;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
+import Blocks3 from './components/Blocks3/Blocks3';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Blocks3/>
   </React.StrictMode>,
   document.getElementById('root')
 );
